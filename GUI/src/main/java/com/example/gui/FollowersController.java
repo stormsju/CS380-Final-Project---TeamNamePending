f8cfeff5c9e74585b9a7138353a1021e0086b542
andrianStepanyuk <aosin2002@gmail.com>
2023-05-30 14:49:46 -0700

    Edited controller classes

--- GUI/src/main/java/com/example/gui/FollowersController.java
+++ GUI/src/main/java/com/example/gui/FollowersController.java
@@ -1,16 +1,4 @@
 package com.example.gui;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class FollowersController extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
+public class FollowersController {
 }
--- GUI/src/main/java/com/example/gui/PrePostController.java
+++ GUI/src/main/java/com/example/gui/PrePostController.java
@@ -1,16 +1,4 @@
 package com.example.gui;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class PrePostController extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
+public class PrePostController {
 }
