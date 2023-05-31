6e0d6cb121c429fbf0bf92a53ad35a31d6ad850e
Wyatt Stohr <wyattcstohr@icloud.com>
2023-05-31 11:08:23 -0700

    Removed picMe1.1

--- PicMe1.1/src/main/java/com/Project/PicMe/repository/PictureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Project.PicMe.repository;
-
-import com.Project.PicMe.entity.Picture;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PictureRepository extends JpaRepository<Picture, Integer>{
-}
