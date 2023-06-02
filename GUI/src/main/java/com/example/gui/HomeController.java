e972b6446997a44f308f1f635b475552e8fd17a3
andrianStepanyuk <aosin2002@gmail.com>
2023-06-02 15:18:33 -0700

    GUI progress
    Contains broken code

--- GUI/src/main/java/com/example/gui/HomeController.java
+++ GUI/src/main/java/com/example/gui/HomeController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import java.io.FileInputStream;
 
 public class HomeController {
 
@@ -19,16 +20,23 @@ public class HomeController {
     private Button btnProfile;
 
     @FXML
-    private ImageView imgFeed;
+    private ImageView imgFeed = new ImageView();
 
     @FXML
     private Label tfCaption;
 
-    public void setPhoto(Image image) {
+    public void setPhoto(String directory) {
 
         // INSERT CODE HERE:
         // Set the image in imgFeed with the given Image.
 
+        try{
+            FileInputStream stream = new FileInputStream(directory);
+            Image image = new Image(stream);
+            imgFeed.setImage(image);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
     public void setCaption(String caption) {
@@ -46,6 +54,7 @@ public class HomeController {
         // Put the file for the photo from the post into imgFeed.setImage(image:Image)
         // Put the caption from the post into tfCaption.setText(caption:string)
 
+        setPhoto("alternatePhoto.jpg");
         ViewManager.switchTo(Views.HOME);
     }
 
